19ed96507be08957c18a04fb24d4adbb2c227d19
DESKTOP-II130FV\DIGAICIA_BADRILO <artem.dehtiarov@nure.ua>
2021-04-05 01:21:50 +0300

    Created class TreeNode with fields and constructors

---
+++ Lab_2/Lab_2/TreeNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class TreeNode
+    {
+        public int data;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode() 
+            : this(0) {}
+        public TreeNode(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
